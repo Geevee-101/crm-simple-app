8618edf6f1e5d59e7b772c6742b3ca7c9965c151
Geevee-101 <gongreashvoid@gmail.com>
2023-11-11 21:14:50 +0800

    Setup Radix UI

--- app/components/AddClient.tsx
+++ app/components/AddClient.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@radix-ui/themes";
+
 const AddClient = () => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
-      Add New Client
-    </button>
+    <div>
+      <Button>Add New Client</Button>
+    </div>
   );
 };
 
