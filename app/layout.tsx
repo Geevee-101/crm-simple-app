bd7a30939bdf2e0fc8ffadae59a09aca6744e8ff
Geevee-101 <gongreashvoid@gmail.com>
2023-11-15 23:28:42 +0800

    Customise Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "CRM-app",
@@ -18,8 +23,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance="light" accentColor="amber">
           <main>{children}</main>
         </Theme>
       </body>
